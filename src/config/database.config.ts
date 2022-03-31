6b571fa7ca6a2bfb74c6ddadfcc799b52dfb3b50
abahernest <abahernesto@gmail.com>
2022-04-01 00:25:28 +0100

    switched to postgres url

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,10 +1,9 @@
 require("dotenv").config();
 import {Sequelize} from "@sequelize/core";
 
-const { DB_HOST,DB_USER,DB_PASSWORD,DB_NAME } = process.env;
+const { DB_HOST,DB_USER,DB_PASSWORD,DB_NAME, DATABASE_URL } = process.env;
 
-const sequelize = new Sequelize(DB_NAME||"",DB_USER||"",DB_PASSWORD||"", {
-    host: DB_HOST,
+const sequelize = new Sequelize(DATABASE_URL||"", {
     dialect:'postgres',
     logging:false
 }
