e13ae775d725ba36268d6dbbb23ad206088d1e03
abahernest <abahernesto@gmail.com>
2022-04-01 17:47:13 +0100

    working tests

---
+++ __tests__/outboundsms.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest';
+import app from '../src/app';
+
+const InboundSMS = {
+    from: '367347882',
+    to: '73849667',
+    text: "OK"
+};
+
+describe('Test Outbound SMS', () => {
+    it('Should return 403 for unauthenticated request', async () => {
+        const result = await request(app).post('/api/v1/outbound/sms').send(InboundSMS);
+        
+        expect(result.status).toEqual(403)
+        expect(result.body.error).toEqual("authentication failed");
+    })
+})
---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+import type {Config} from '@jest/types'
+
+const config: Config.InitialOptions = {
+    verbose: true,
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    }
+}
+
+export default config;
